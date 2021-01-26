9413d3432bbd7ffb6a0f8e7dc071c2d42a67c6fb
Varit Seekhao <varit.seekhao@gmail.com>
2021-01-25 21:48:59 -0500

    update css

--- src/components/Name/Name.jsx
+++ src/components/Name/Name.jsx
@@ -6,26 +6,28 @@ function Name(props) {
 
   return (
     <div className="primary-container">
-    <div className="agents-name-container">
       <div className="agent-scrollbox">
         {agents.map((agent) => {
           return (
             <div className="agent" key={agent.id}>
               <ul className="agents-id">
-                <li>{agent.fields.id}
-                </li>
+                <li>{agent.fields.id}</li>
               </ul>
               <ul className="agents-name">
                 <li>
-                  <NavLink activeStyle={{color: "#ff4655"}} to={`/${agent.id}/agent/${agent.fields.name}`}>{agent.fields.name.toUpperCase()}</NavLink>
+                  <NavLink
+                    activeStyle={{ color: "#ff4655" }}
+                    to={`/${agent.id}/agent/${agent.fields.name}`}
+                  >
+                    {agent.fields.name.toUpperCase()}
+                  </NavLink>
                 </li>
               </ul>
             </div>
           );
         })}
       </div>
-    </div>
-    <div className="secondary-container"></div>
+      <div className="secondary-container"></div>
     </div>
   );
 }
