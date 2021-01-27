eab330472eb94df0a099848bc2da03d6aa775727
Varit Seekhao <varit.seekhao@gmail.com>
2021-01-26 23:20:02 -0500

    add functioning horizontal scroll for mobile device

--- src/components/Name/Name.jsx
+++ src/components/Name/Name.jsx
@@ -21,25 +21,29 @@ function Name(props) {
         </video>
       </div>
       <div className="agent-scrollbox">
-        {agents.map((agent) => {
-          return (
-            <div className="agent" key={agent.id}>
-              <ul className="agents-id">
-                <li>{agent.fields.id}</li>
-              </ul>
-              <ul className="agents-name">
-                <li>
-                  <NavLink
-                    activeStyle={{ color: "#ff4655" }}
-                    to={`/${agent.id}/agent/${agent.fields.name}`}
-                  >
-                    {agent.fields.name.toUpperCase()}
-                  </NavLink>
-                </li>
-              </ul>
-            </div>
-          );
-        })}
+        <div className="horizontal-scroll-container">
+          {agents.map((agent) => {
+            return (
+              <div className="agent" key={agent.id}>
+                <div className="horizontal-agent-container">
+                <ul className="agents-id">
+                  <li>{agent.fields.id}</li>
+                </ul>
+                <ul className="agents-name">
+                  <li>
+                    <NavLink
+                      activeStyle={{ color: "#ff4655" }}
+                      to={`/${agent.id}/agent/${agent.fields.name}`}
+                    >
+                      {agent.fields.name.toUpperCase()}
+                    </NavLink>
+                  </li>
+                </ul>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
